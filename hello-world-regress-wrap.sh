fae01e6efcc4a0cf4e96941f909dd67ff1f74008
Pranau-R <pranau@mcci.com>
2024-03-18 14:49:12 +0530

    fixed typo

--- hello-world-regress-wrap.sh
+++ hello-world-regress-wrap.sh
@@ -240,7 +240,7 @@ function _main {
     _init "$@"
 
     _setup_path
-    _setup_hello-world-arduino libraries/hello-world-arduino
+    _setup_hello_world_arduino libraries/hello-world-arduino
     _setup_arduino_cli
     for iArch in ${MCCI_ARDUINO_FQCNS[$OPTARCH]} ; do
         _setup_board_package "$iArch"
